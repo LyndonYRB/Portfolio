98bf4ae4d638e717fa602514a4fe5fba714674ca
Lyndon <lyndon.stluce@gmail.com>
2022-05-17 11:30:33 -0400

    updated

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,13 +1,44 @@
 import React from "react";
+import ImageOne from "../images/1a.png"
+import ImageTwo from "../images/1b.png"
+import ImageThree from "../images/2a.png"
+import ImageFour from "../images/2b.png"
+import ImageFive from "../images/2c.png"
+import "./Projects.css"
 
 export default function Projects() {
   
   return (
     <div>Projects
       <ul>
-      Project 1
-      Project 2
-      Project 3
+        <li>
+          <a href="https://lyndons-rando-country-generator.netlify.app/">Project 1</a>
+          
+          <h4>Random Country Genenator</h4>
+          <img src={ImageOne}/><img src={ImageTwo}/>
+          <p>
+            You click the button and the name,
+            capital and the popaltion of a random
+            country will pop up. The API I used was made
+            by one of my classmates, It was created using
+            MongoDB for the front-end I used React.
+          </p>
+        </li>
+        <li>
+          <a href="https://lyndonyrb.github.io/fly-swatter/">Project 2</a>
+          
+          <h4>Fly Swatter</h4>
+          <img className="img-two" src={ImageThree}/><img className="img-two" src={ImageFour}/><img className="img-two" src={ImageFive}/>
+          <p>A simple game that utilize the mouse
+            click and collision detection, made in JavaScript,
+            HTML and CSS. The objective is to click on the fly to
+            kill it before it touches the cake. Evertime you win and
+            opt to play again an extra fly will be added making the
+            game more difficult.</p>
+        </li>
+        <li>
+        <p>Project 3 coming soon</p>
+        </li>
       </ul>
     </div>
   )
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,4 +1,15 @@
 import React from "react"
+import "./Skills.css"
+import ImgHTML from "../images/html.png"
+import ImgJS from "../images/js.png"
+import ImgCss from "../images/css.png"
+import ImgReact from "../images/react.png"
+import ImgMongoDB from "../images/mongodb.png"
+import ImgExpress from "../images/express.png"
+import ImgPython from "../images/python.png"
+import ImgDjango from "../images/django.png"
+import ImgSQL from "../images/sql.jpg"
+
 
 export default function Skills() {
   
@@ -15,7 +26,18 @@ export default function Skills() {
                   PYTHON
                   DJANGO
                   SQL
-                </div>
+      </div>
+      <div className="grid">
+      <div className="cell"> <img src={ImgHTML}/> </div>
+      <div className="cell"> <img src={ImgJS}/> </div>
+       <div className="cell"> <img src={ImgCss} /> </div>
+        <div className="cell"> <img src={ImgReact} /> </div>
+        <div className="cell"> <img src={ImgMongoDB} /> </div>
+        <div className="cell"> <img src={ImgExpress} /> </div>
+        <div className="cell"> <img src={ImgPython} /> </div>
+        <div className="cell"> <img src={ImgDjango} /> </div>
+        <div className="cell"> <img src={ImgSQL}/> </div>
+      </div>
 
 
     </div>
