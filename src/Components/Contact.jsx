08acd4c33236c06ce69a9bab88d1879268e5f46d
Lyndon <lyndon.stluce@gmail.com>
2022-05-17 11:48:53 -0400

    resume

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+
 export default function Contact() {
   
   return (
     <div>
       <h2>Contact Info</h2> 
       <ul>
-        Resume
+        <li><a href="https://drive.google.com/file/d/10qs9hkFxTMqsHEV1wn4WdZHQ29fmoyon/view">
+          Resume</a></li>
        <li><a href="https://github.com/LyndonYRB"> Github</a></li>
        <li><a href="https://www.linkedin.com/in/lyndon-stluce/">LinkedIn</a></li>
       </ul>
