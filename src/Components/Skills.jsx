04b2e2b28c556cb0285754d8a6208d9a0e693518
Lyndon <lyndon.stluce@gmail.com>
2022-05-17 15:20:14 -0400

    fixed skills page

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -8,7 +8,7 @@ import ImgMongoDB from "../images/mongodb.png"
 import ImgExpress from "../images/express.png"
 import ImgPython from "../images/python.png"
 import ImgDjango from "../images/django.png"
-import ImgSQL from "../images/sql.jpg"
+import ImgSQL from "../images/sql.png"
 
 
 export default function Skills() {
@@ -16,27 +16,19 @@ export default function Skills() {
   return (
     <div>
       Skills
-                <div>
-                  HTML5
-                  JAVASCRIPT
-                  css
-                  React
-                  MONGODB
-                  EXPRESS
-                  PYTHON
-                  DJANGO
-                  SQL
-      </div>
       <div className="grid">
       <div className="cell"> <img src={ImgHTML}/> </div>
       <div className="cell"> <img src={ImgJS}/> </div>
        <div className="cell"> <img src={ImgCss} /> </div>
-        <div className="cell"> <img src={ImgReact} /> </div>
+        <div className="cell"> <img src={ImgReact} />  </div>
         <div className="cell"> <img src={ImgMongoDB} /> </div>
         <div className="cell"> <img src={ImgExpress} /> </div>
         <div className="cell"> <img src={ImgPython} /> </div>
-        <div className="cell"> <img src={ImgDjango} /> </div>
-        <div className="cell"> <img src={ImgSQL}/> </div>
+        <div className="cell"> <img src={ImgDjango} />  </div>
+        <div className="cell"> <img src={ImgSQL} /> </div>
+        <div>
+                
+      </div>
       </div>
 
 
